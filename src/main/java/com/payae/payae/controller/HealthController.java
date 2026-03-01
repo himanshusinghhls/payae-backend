e00f98250d979391ecfdf3acf1f5c1c249320549
Himanshu Singh <142795088+himanshusinghhls@users.noreply.github.com>
2026-03-01 12:59:04 +0530

    Add health endpoint

---
+++ src/main/java/com/payae/payae/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.payae.payae.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "PayAE Backend Running 🚀";
+    }
+}
